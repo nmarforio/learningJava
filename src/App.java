10cbcabf037a9003b43f5fd6bc7d8698deb44661
python-game <n.marforio@gmail.com>
2023-08-16 10:01:50 +0200

    keep with beginner Java learning

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import java.awt.Point;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Scanner;
 
 public class App {
     // TO RUN JAVA USE F5
@@ -45,12 +46,36 @@ public class App {
         
         int[] numbers2 = {1,2,3,4,5};
         System.out.println(numbers2);
-        //// if you don't dot arraytostring and just put numbers you are gonna see the adress of the memorie that reference to the array
+        // if you don't dot arraytostring and just put numbers you are gonna see the adress of the memorie that reference to the array
+    
+       // you can crate a 2 dimantional arrays 
+       int [][] numbers3 = new int[2][3];// 2 rows and 3 columns
+       numbers3[0][0] = 1;
+       System.out.println(Arrays.deepToString(numbers3)); // use deep for multidimentional arrays
+       // or you can do it so
+       int [][]numbers4 = {{1,2,3}, {4,5,6}};
+       System.out.println(Arrays.deepToString(numbers4));
 
-       
-        
-       
+       //CONSTANT const = final, use CAPITAL LETTER TO DEFINE A CONSTANT
+       final float PI = 3.14F; // like that is immutable
 
+       // ARITMETICS: for division if is a float you have to do so
+       double result = (double)10 / (double)3;
+       System.out.println(result);
+
+       // x = 1; x++ = 2; x += 2 add 2 to x
+       // you cannot add a string to a integer or float so you use parseInt/parseFloat etc
+
+       // READING INPUT
+       Scanner scanner = new Scanner(System.in);
+       System.out.print("age: ");
+       byte età = scanner.nextByte();
+       System.out.println("you are " + età);
+       //
+       Scanner yourname = new Scanner(System.in);
+       System.out.print("Name: "); //using print isnt creating the new line under name but it stay at the same level
+       String name = yourname.next(); //using next like this is only storeing one token, if you put you full name only the first one is print out
+       System.out.println("you are " + name);
+       // use nextLine to printout all the token in the same line
     }
 }
- 
