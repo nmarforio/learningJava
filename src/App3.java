a145efe14b26764698c74d695a4c95ff5f0ac280
python-game <n.marforio@gmail.com>
2023-09-14 13:53:46 +0200

    learning Java

--- src/App3.java
+++ src/App3.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 //you can create many classes and at the end with F5 you decide wich one to run if is in the same page like 
 // in this file I have App3 and Car!!
@@ -155,3 +161,134 @@ class Maininterface{
         myFriend.iAmAlone();
     }
 }
+
+// Enums is an unmutable value has to be UPPERCASE
+
+class ForEnums{
+    enum Level{
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+public static void main(String[] args) {
+    Level myVar = Level.HIGH;
+    
+   switch (myVar) {
+    case LOW:
+        System.out.println("low level");
+        break;
+   
+    case HIGH:
+        System.out.println("high level");
+        break;
+    case MEDIUM:
+        System.out.println("medium level");
+   }
+
+   // with for loop
+        for (Level myLevel : Level.values()){
+            System.out.println(myLevel);
+        }
+}
+}
+
+class NewInput{
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner (System.in); // in for input
+        System.out.println("your astrologic sign?");
+
+        String astrolocigSign = myScanner.nextLine(); // nextBoolean(), nextInt(),next... many more type
+        System.out.println(astrolocigSign + " is my sign");
+
+        System.out.println("how many country did you visit this year?");
+        int countryNumber = myScanner.nextInt();
+        System.out.println("I visit " + countryNumber + " this year");
+    }
+}
+
+
+class NewTime{ // import java.time.LocalDate
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+    }
+}
+class FormattingDate{ //import time.format.DateTimeFormatter
+    public static void main(String[] args) {
+        LocalDateTime beforeFormatting = LocalDateTime.now();
+        System.out.println("No formatting: "+beforeFormatting);
+
+        // with ofPattern you can decide how to show the date mm/dd/yy, HH:mm:ss, E(shows the day),mmm,yyyy
+        DateTimeFormatter formattingDate = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        String afterFormatting = beforeFormatting.format(formattingDate);
+        System.out.println("Yes formatting: "+afterFormatting);
+
+    }
+}
+
+class ArraysListEx{ // import ArrayList
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("volvo");
+        cars.add("BMW");
+        cars.add("Mercedes");
+        System.out.println(cars);
+
+        for(int i = 0; i < cars.size(); i++){
+            System.out.println("for loop " + cars.get(i));
+        }
+
+        System.out.println(cars.get(0));
+        cars.set(0,"Toyota"); // used to modify an element NO ADD!
+        System.out.println("after set " +cars); // with this sout i can see the modified array 
+
+        cars.remove(0);
+        System.out.println("after remove "+ cars);
+        cars.clear();
+        System.out.println("after clear " +cars);
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(344);
+        numbers.add(78);
+        numbers.add(213);
+        numbers.add(2);
+
+        // import Collections
+        Collections.sort(numbers);
+        System.out.println(numbers); 
+        
+    }
+}
+class HashMapEx{ // import HashMap
+        public static void main(String[] args) {
+
+            // the first string is the key, second is the acutal element of the array
+            // int Hashmao key and value are actually objects
+            // get or remove you use the key 
+            HashMap<String, String> capitalCities = new HashMap<String,String>();
+            capitalCities.put("england","london");
+            capitalCities.put("germany", "berlin");
+            capitalCities.put("brasil", "brasilia");
+            System.out.println(capitalCities);
+
+            
+            // this are forEach loops
+            for(String i : capitalCities.keySet()){
+                System.out.println("hier the keys " + i);
+            }
+            for(String i : capitalCities.values()){
+                System.out.println("this are the values "+i);
+            } 
+            
+            HashMap<String, Integer> personAge = new HashMap<String, Integer>();
+            personAge.put("Alex",22);
+            personAge.put("Nicole", 3);
+            personAge.put("Franzi",12);
+
+            for (String i : personAge.keySet()){
+                System.out.println("name " + i + " age " + personAge.get(i));
+            }
+        }
+        
+    }
